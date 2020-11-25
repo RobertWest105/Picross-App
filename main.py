2d3f049a195ca0561abfc1101d58788ab83c3186
Robert West <robert.w.main@gmail.com>
2020-11-25 02:21:25 +0000

    Initial commit - all main functionality (random puzzles and puzzles from images)

---
+++ main.py
@@ -0,0 +1,362 @@
+import pygame
+import os
+import random
+import math
+import time
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from photoToPicross import imageToPicross
+
+pygame.font.init()
+
+###### Window setup
+WIDTH, HEIGHT = 850, 650
+bottomBarHeight = HEIGHT // 10
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (150, 150, 150)
+RED = (255, 0, 0)
+
+FONT_SIZE = 20
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Picross Game")
+
+BG = pygame.transform.scale(pygame.image.load(os.path.join("PicrossAppAssets", "PicrossBackground.png")), (WIDTH, HEIGHT))
+
+
+class Board:
+    def __init__(self, boardOrigin, gridWidth, gridHeight, sqSize=30):
+        self.origin = boardOrigin
+        self.cols = gridWidth
+        self.rows = gridHeight
+        self.sqSize = sqSize
+
+        # self.rowClues, self.colClues = makeRandPuzzle(gridWidth, gridHeight)
+
+        self.squares = [[Square(self.origin[0] + j * sqSize, self.origin[1] + i * sqSize, sqSize) for j in range(gridWidth)] for i in range(gridHeight)]
+
+    def draw(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.squares[i][j].draw()
+
+    def makeSquaresChangeable(self):
+        for row in self.squares:
+            for sq in row:
+                sq.stateChangeable = True
+
+
+class Square:
+    def __init__(self, posX, posY, size=40):
+        self.x = posX
+        self.y = posY
+        self.size = size
+        self.state = 0
+        self.xSprite = pygame.transform.scale(pygame.image.load(os.path.join("PicrossAppAssets", "XSquare.png")), (self.size - 2, self.size - 2))
+        self.stateChangeable = True
+
+    def draw(self):
+        if self.state == 0 or self.state == 1:
+            pygame.draw.rect(WINDOW, WHITE if self.state == 0 else BLUE, (self.x, self.y, self.size - 2, self.size - 2))
+        else:
+            WINDOW.blit(self.xSprite, (self.x, self.y))
+        # print("drew " + str(self.x) + ", " + str(self.y))
+
+    def setState(self, newState):
+        if self.stateChangeable:
+            if self.state != newState and self.state < 2:
+                self.state = newState
+            else:
+                self.state = 0
+            self.stateChangeable = False
+
+
+def getClues(line):
+    clues = []
+    contiguousCount = 0
+    for p in line:
+        if p == 1:
+            contiguousCount += 1
+        elif contiguousCount != 0:
+            clues.append(contiguousCount)
+            contiguousCount = 0
+    if contiguousCount != 0:
+        clues.append(contiguousCount)
+    if len(clues) == 0:
+        clues.append(0)
+    return clues
+
+
+def makeRandPuzzle(width, height):
+    rows = [[random.randint(0, 1) for j in range(width)] for i in range(height)]
+    cols = [[row[j] for row in rows] for j in range(width)]
+
+    print(rows)
+    print(cols)
+
+    return (rows, cols)
+
+
+def checkLine(line, clues):
+    return getClues(line) == clues
+
+
+def createButton(picture, centre):
+    image = pygame.image.load(picture)
+    imagerect = image.get_rect()
+    imagerect.center = centre
+    WINDOW.blit(image, imagerect)
+    return imagerect
+
+
+def mouseInRect(rect, mousePos):
+    return rect.topleft[0] <= mousePos[0] <= rect.bottomright[0] and rect.topleft[1] <= mousePos[1] <= rect.bottomright[1]
+
+
+def main(mode, randomSize=None, imageFile=None):
+    run = True
+    fps = 60
+    clock = pygame.time.Clock()
+
+    mainFont = pygame.font.SysFont("calibri", FONT_SIZE)
+    solvedFont = pygame.font.SysFont("calibri", FONT_SIZE*2)
+
+    rows, cols = (0, 0)
+
+    if mode == "image" and imageFile is not None:
+        try:
+            targetHeight = 20
+            rows, cols = imageToPicross(imageFile, targetHeight)
+        except Exception as e:
+            # Unable to create puzzle from image
+            print(e)
+            return
+    elif mode == "random" and randomSize is not None:
+        rows, cols = makeRandPuzzle(randomSize[0], randomSize[1])
+    else:
+        # Invalid mode selected
+        return
+
+    gridWidth = len(cols)
+    gridHeight = len(rows)
+
+    rowClues = [getClues(row) for row in rows]
+    colClues = [getClues(col) for col in cols]
+
+    def totalDigitsInArray(arr):
+        digitsTotal = 0
+        for num in arr:
+            digitsTotal += (1 + (int(math.log10(num)) if num > 0 else 0))
+        return digitsTotal
+
+    cluesWidth = max([totalDigitsInArray(rowClues[i]) for i in range(gridHeight)]) * FONT_SIZE
+    cluesHeight = max([len(colClues[j]) for j in range(gridWidth)]) * FONT_SIZE
+
+    maxSqSize = 40
+    sqSize = int(min((WIDTH - cluesWidth) // gridWidth, (HEIGHT - cluesHeight - bottomBarHeight) // gridHeight, maxSqSize))
+
+    board = Board((cluesWidth, cluesHeight), gridWidth, gridHeight, sqSize)
+
+    solvedRows = [False for i in range(gridHeight)]
+    solvedCols = [False for i in range(gridWidth)]
+    solved = False
+
+    solvedTimer = 0
+
+    print(rowClues)
+    print(colClues)
+
+    # for i in range(gridWidth * gridHeight):
+    #     grid.append(pygame.draw.rect(WINDOW, squareColour, (
+    #     cluesWidth + (i % gridWidth) * sqSize, cluesHeight + (i // gridWidth) * sqSize, sqSize - 2, sqSize - 2)))
+
+    # Display background
+    WINDOW.blit(BG, (0, 0))
+
+    # Display row clues
+    for i in range(len(rowClues)):
+        digitsSoFar = 0
+        for clue in reversed(rowClues[i]):
+            clueText = mainFont.render(f"{clue}", 1, (255, 255, 255))
+            digitsSoFar += (1 + (int(math.log10(clue)) if clue > 0 else 0))
+            WINDOW.blit(clueText, (cluesWidth - int(digitsSoFar * FONT_SIZE * 0.65), cluesHeight + int(0.2 * sqSize) + i * sqSize))
+
+    # Display col clues
+    for i in range(len(colClues)):
+        index = 0
+        for clue in reversed(colClues[i]):
+            clueText = mainFont.render(f"{clue}", 1, (255, 255, 255))
+            digits = 1 + (int(math.log10(clue)) if clue > 0 else 0)
+            WINDOW.blit(clueText,
+                        (cluesWidth + int(i * sqSize + (0.3*sqSize if digits == 1 else 0)), cluesHeight - FONT_SIZE - index * FONT_SIZE))
+            index += 1
+
+    # TODO: Display 'back to main menu' and 'reset puzzle' buttons
+
+
+    def redrawWindow():  # make OO with each square an object with position, size, type(empty, full, X)
+        # if sqX != -1 and sqY != -1:
+        #     pygame.draw.rect(WINDOW, squareColour, (
+        #         cluesWidth + sqX*sqSize, cluesHeight + sqY*sqSize, sqSize - 2, sqSize - 2))
+
+        board.draw()
+
+        if solved:
+            solvedText = solvedFont.render("Complete!", 1, RED)
+            WINDOW.blit(solvedText, (WIDTH / 2 - solvedText.get_width() / 2, HEIGHT / 2))
+
+        pygame.display.update()
+
+    mouseDown = False
+
+    while run:
+        clock.tick(fps)
+        redrawWindow()
+
+        if solved:
+            solvedTimer += 1
+            if solvedTimer > fps * 2:
+                run = False
+            else:
+                continue
+
+        ## Check if puzzle is solved
+        for rowIndex in range(len(board.squares)):
+            solvedRows[rowIndex] = checkLine([sq.state for sq in board.squares[rowIndex]], rowClues[rowIndex])
+        i = 0
+        for col in [[row[j] for row in board.squares] for j in range(gridWidth)]:
+            solvedCols[i] = checkLine([sq.state for sq in col], colClues[i])
+            i += 1
+        solved = all(solvedRows) and all(solvedCols)
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    quit()
+            if event.type == pygame.QUIT:
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN or mouseDown: #pygame.mouse.get_pressed()[0] or pygame.mouse.get_pressed()[2]:
+                #print(event)
+                mouseDown = True
+                pos = pygame.mouse.get_pos()
+                if pos[0] > cluesWidth and pos[0] < cluesWidth + gridWidth * sqSize and pos[1] > cluesHeight and pos[1] < cluesHeight + gridHeight * sqSize:
+                    colClicked = int((pos[0] - cluesWidth) // sqSize)
+                    rowClicked = int((pos[1] - cluesHeight) // sqSize)
+
+                    if pygame.mouse.get_pressed()[0]:  # left click = 1st element in returned tuple
+                        print(f"left clicked on square {colClicked}, {rowClicked}")
+                        board.squares[rowClicked][colClicked].setState(1)  # state 1 means filled
+                    elif pygame.mouse.get_pressed()[2]:  # right click = 3rd element
+                        print(f"right clicked on square {colClicked}, {rowClicked}")
+                        board.squares[rowClicked][colClicked].setState(2)  # any other state number means 'x' / grey
+                ## TODO: add functionality for restart and return to main menu buttons
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouseDown = False
+                board.makeSquaresChangeable()
+                #print("squares now changable")
+
+def mainMenu():
+    titleFont = pygame.font.SysFont("calibri", FONT_SIZE*3)
+    menuTextFont = pygame.font.SysFont("calibri", FONT_SIZE)
+
+    run = True
+
+    modeChoice = None
+
+    selectedWidth, selectedHeight = (0, 0)
+
+    while run:
+        WINDOW.blit(BG, (0, 0))
+
+        title = titleFont.render("PICROSS", 1, WHITE)
+        WINDOW.blit(title, (WIDTH/2 - title.get_width()/2, HEIGHT/5))
+
+        numberButtonWidth, numberButtonHeight = (42, 42)
+
+        widthText = menuTextFont.render("Choose width: ", 1, WHITE)
+        heightText = menuTextFont.render("Choose height: ", 1, WHITE)
+        WINDOW.blit(widthText, (WIDTH/4 - widthText.get_width() - numberButtonWidth/2, HEIGHT/3 - numberButtonHeight/4))
+        WINDOW.blit(heightText, (WIDTH/4 - widthText.get_width() - numberButtonWidth/2, HEIGHT/3 + 3*numberButtonHeight/4))
+
+        widthButtons = [
+            createButton(os.path.join("PicrossAppAssets", "5Button.png"), (WIDTH/4, HEIGHT/3)),
+            createButton(os.path.join("PicrossAppAssets", "10Button.png"), (WIDTH/4 + numberButtonWidth, HEIGHT/3)),
+            createButton(os.path.join("PicrossAppAssets", "15Button.png"), (WIDTH/4 + 2*numberButtonWidth, HEIGHT/3)),
+            createButton(os.path.join("PicrossAppAssets", "20Button.png"), (WIDTH/4 + 3*numberButtonWidth, HEIGHT/3))
+        ]
+        heightButtons = [
+            createButton(os.path.join("PicrossAppAssets", "5Button.png"), (WIDTH/4, HEIGHT/3 + numberButtonHeight)),
+            createButton(os.path.join("PicrossAppAssets", "10Button.png"), (WIDTH/4 + numberButtonWidth, HEIGHT/3 + numberButtonHeight)),
+            createButton(os.path.join("PicrossAppAssets", "15Button.png"), (WIDTH/4 + 2*numberButtonWidth, HEIGHT/3 + numberButtonHeight)),
+            createButton(os.path.join("PicrossAppAssets", "20Button.png"), (WIDTH/4 + 3*numberButtonWidth, HEIGHT/3 + numberButtonHeight))
+        ]
+
+        randomButtonEnabled = selectedWidth != 0 and selectedHeight != 0
+        randomPuzzleButton = createButton(os.path.join("PicrossAppAssets", "RandomPuzzleButton.png"), (WIDTH / 3, HEIGHT / 2))
+
+        imagePuzzleButton = createButton(os.path.join("PicrossAppAssets", "FromImageButton.png"), (2*WIDTH/3, HEIGHT/2))
+
+        instructionText = menuTextFont.render("How to play:", 1, WHITE)
+        textLine1 = menuTextFont.render("Satisfy all row and column clues by filling squares, leaving gaps between contiguous blocks", 1, WHITE)
+        textLine2 = menuTextFont.render("Left click to fill squares", 1, WHITE)
+        textLine3 = menuTextFont.render("Right click to mark squares as empty", 1, WHITE)
+
+        WINDOW.blit(instructionText, (WIDTH/2 - instructionText.get_width()/2, 2*HEIGHT/3))
+        WINDOW.blit(textLine1, (WIDTH/2 - textLine1.get_width()/2, 2*HEIGHT/3 + instructionText.get_height()))
+        WINDOW.blit(textLine2, (WIDTH/2 - textLine2.get_width()/2, 2*HEIGHT/3 + instructionText.get_height() + textLine1.get_height()))
+        WINDOW.blit(textLine3, (WIDTH/2 - textLine3.get_width()/2, 2*HEIGHT/3 + instructionText.get_height() + textLine1.get_height() + textLine2.get_height()))
+        pygame.display.update()
+
+        if modeChoice == "random":
+            modeChoice = None
+            main("random", randomSize=(selectedWidth, selectedHeight))
+        elif modeChoice == "image":
+            modeChoice = None
+            viableImageTypes = [("Image Files", ("*.png", "*.jpg"))]
+            imageFileName = askopenfilename(initialdir=os.getcwd(), title="Select Image To Create Puzzle From",
+                                            filetypes=viableImageTypes)
+            tk.Tk().destroy()
+            # print(imageFile)
+            main("image", imageFile=imageFileName)
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mousePos = pygame.mouse.get_pos()
+                if mouseInRect(randomPuzzleButton, mousePos) and randomButtonEnabled:
+                    print("rand button clicked!")
+                    modeChoice = "random"
+                elif mouseInRect(imagePuzzleButton, mousePos):
+                    modeChoice = "image"
+                elif mouseInRect(widthButtons[0], mousePos):
+                    selectedWidth = 5
+                elif mouseInRect(widthButtons[1], mousePos):
+                    selectedWidth = 10
+                elif mouseInRect(widthButtons[2], mousePos):
+                    selectedWidth = 15
+                elif mouseInRect(widthButtons[3], mousePos):
+                    selectedWidth = 20
+                elif mouseInRect(heightButtons[0], mousePos):
+                    selectedHeight = 5
+                elif mouseInRect(heightButtons[1], mousePos):
+                    selectedHeight = 10
+                elif mouseInRect(heightButtons[2], mousePos):
+                    selectedHeight = 15
+                elif mouseInRect(heightButtons[3], mousePos):
+                    selectedHeight = 20
+
+                print(f"width: {selectedWidth}, height: {selectedHeight}")
+
+    pygame.quit()
+
+
+tk.Tk().withdraw()
+mainMenu()
---
+++ photoToPicross.py
@@ -0,0 +1,68 @@
+from PIL import Image
+import os
+import math
+import numpy as np
+
+imgFolder = "./testImages/"
+resultsFolder = "./resultPuzzles/"
+
+posSizes = [(5,5), (10,10), (15,15), (20,20), (25,25)]
+
+targetHeight = 20
+
+def closestPuzzleSize(currentSize, possibleSizes):
+    differences = []
+    for posSize in possibleSizes:
+        xDiff = abs(currentSize[0] - posSize[0])
+        yDiff = abs(currentSize[1] - posSize[1])
+        diff = math.sqrt(xDiff*xDiff + yDiff*yDiff)
+        differences.append(diff)
+
+    minDiff = min(differences)
+    return possibleSizes[differences.index(minDiff)]
+
+def pixelate(img):
+    imgSmall = img.reduce(4)
+
+    imgMaxSize = max(img.width, img.height)
+    pixelImg = imgSmall.resize(img.size, Image.NEAREST)
+
+    return pixelImg
+
+def imageToPicross(imagePath, targetHeight):
+    testImg = Image.open(imagePath)
+    # testBW = testImg.convert(mode="1")
+
+    # imgSmall = testGreyscale.reduce(4) #testImg.resize((16, 16), Image.BILINEAR)
+    # pixelTest = pixelate(testImg)
+
+    ratioWidthToHeight = testImg.width / testImg.height
+    puzzleSizeImg = testImg.resize((int(targetHeight * ratioWidthToHeight), targetHeight), Image.NEAREST)
+
+    threshold = np.mean(puzzleSizeImg)
+    func = lambda x: 255 if x > threshold else 0
+
+    imgGrey = puzzleSizeImg.convert(mode="L")  # Convert to greyscale
+    puzzleImg = imgGrey.point(func, mode="1")
+
+    # puzzleImgData = np.asarray(imgGrey)
+    # thresholdedData = (puzzleImgData > threshold) * 1.0
+
+    # plt.imshow(thresholdedData)
+    # plt.show()
+
+    # [[not row[p] for p in row] for row in np.asarray(puzzleImg)]
+
+    puzzleRows = (~np.asarray(puzzleImg) * 1).tolist()
+    puzzleCols = [[row[j] for row in puzzleRows] for j in range(puzzleImg.width)]
+
+    return (puzzleRows, puzzleCols)
+
+    # # Save monochrome puzzle image for testing
+    # puzzleImgName = resultsFolder + "Picross " + testImgName
+    # print(puzzleImgName + "\n" + str(puzzleRows) + "\n" + str(puzzleCols))
+    # if not os.path.exists(puzzleImgName):
+    #   puzzleImg.save(puzzleImgName)
+
+if not os.path.exists(resultsFolder):
+    os.makedirs(resultsFolder)
